8288bfcebb26876e074a9d40f18902231ef48399
Albert Yue <albert.s.yue@gmail.com>
2019-11-29 11:53:41 -0500

    Add another abstract method to entity

--- renderer/entity.py
+++ renderer/entity.py
@@ -8,6 +8,16 @@ class Entity(abc.ABC):
         self.velocity = velocity
         self.id = -1
 
+    @abc.abstractmethod
+    def _make_shape(self):
+        """
+        Makes the pymunk.Shape object that the entity is associated with
+
+        Should be called in __init__, and generally called at the end
+        after other fields are set
+        """
+        pass
+
     def get_shape(self):
         return self.shape
 
