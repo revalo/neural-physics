b535bf3bd8440fd61835ee57c897e531f0e852c4
Alex Chen <achen19@mit.edu>
2019-11-30 15:16:11 -0800

    make rectangles stack

--- show_world.py
+++ show_world.py
@@ -2,7 +2,7 @@ from renderer.threecircles import ThreeCircles
 from renderer.rectangles import Rectangles
 
 if __name__ == "__main__":
-    scene = ThreeCircles(headless=False, radius=30)
+    scene = Rectangles(headless=False)
 
     for frame in range(1000):
         scene.step()
